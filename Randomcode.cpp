860daa2f2cc70b843934830f38316686c386fa77
Spoon <58980129+DualSpoonMaster@users.noreply.github.com>
2021-10-28 09:31:49 -0400

    Create Randomcode.cpp

---
+++ Randomcode.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+ main(){
+   cout<<"Hi whats your name: ";
+   string name;
+   cin>>name;
+   cout<<endl;
+   cout<<"HI"<<name<<"nice to meet you!";
+   cout<<"How old are you!: ";
+   int age;
+   cin>>age;
+   //kinda sounds creepy like the man in the white van at the end of my street!//
+   cout<<"Wow Your "<<age<<" Years old!!";
